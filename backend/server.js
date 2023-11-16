d756f28a50348cce4cb81f3d8d5f589f60dbdf6b
Akul16 <akuldpatel16@gmail.com>
2023-11-16 18:54:14 -0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    });
+}
+
+
+app.use(errorHandler);
+app.listen(port, () => console.log(`server running on ${port}`));
